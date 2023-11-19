0a8ee434222fbf4060963f8a4b0aa267f3920b19
Ryan Diaz <ryandiazgithub@gmail.com>
2023-11-19 16:49:40 -0600

    Leptos Getting Started Book:     - Building User Interfaces::Dynamic Attributes 3.2

--- src/main.rs
+++ src/main.rs
@@ -3,34 +3,28 @@ use leptos::*;
 #[component]
 fn App() -> impl IntoView {
     let (count, set_count)= create_signal(0);
+    let double_count = move || count() * 2;
+
 
     view! {
         <button
+            class=("red", move || count() % 2 == 1)
             on:click=move |_| {
-                set_count.update(|n| *n += 2);
+                set_count.update(|n| *n += 1);
             }
         >
             "Click me: "
-            {count}
+            {move || count()}
         </button>
+        <progress
+            max="50"
+            // signals are functions, so this <=> `move || count.get()`
+            value=double_count
+        />
         <p>
-            <strong>"Reactive: "</strong>
-            // you can insert Rust expressions as values in the DOM
-            // by wrapping them in curly braces
-            // if you pass in a function, it will reactively update
-            {move || count.get()}
-        </p>
-
-        <p>
-            <strong>"Reactive shorthand: "</strong>
-            // signals are functions, so we can remove the wrapping closure
-            {count}
-        </p>
-        <p>
-            <strong>"Not reactive: "</strong>
-            // Note: if you write {count()}, this will *not* be reactive
-            // it simply gets the value of count once.
-            {count()}
+            "Double Counts: "
+            // and again here
+            {double_count}
         </p>
     }
 }
