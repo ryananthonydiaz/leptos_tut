ccef1206ebc5c3c69bb4b9b022d14315e53ac476
Ryan Diaz <ryandiazgithub@gmail.com>
2023-11-19 22:03:36 -0600

    Leptos Book Getting Started:     - Building User Interfaces::Forms and Inputs.     - Uncontrolled Inputs.

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,40 @@
 use leptos::*;
+use leptos::html::Input;
+use leptos::ev::SubmitEvent;
 
 #[component]
 fn App() -> impl IntoView {
     let name: ReadSignal<String>;
     let set_name: WriteSignal<String>;
-    (name, set_name) = create_signal("Controlled".to_string());
-    view! {
-        <input
-            type="text"
-            on:input=move |ev| {
-                // event_target_value is a Leptos helper function
-                // it functions the same way as event.target.value
-                // in JavaScript, but smooths out some of the typecasting
-                // necessary to make this work in Rust
-                set_name(event_target_value(&ev));
-            }
-            
-            // the `prop:` syntax lets you update a DOM property,
-            // rather than an attribute.
-            prop:value=name
+    (name, set_name) = create_signal("Uncontrolled".to_string());
 
-        />
-        <p>"Name is: " {name}</p> 
+    let input_element: NodeRef<Input> = create_node_ref();
+
+    let on_submit = move |ev: SubmitEvent| {
+        // stop the page from reloading!
+        ev.prevent_default();
+
+        // here, we'll extract the value from the input
+        let value = input_element()
+            // event handlers can only fire after the view
+            // is mounted to the DOM, so the `NodeRef` will be `Some`
+            .expect("<input> to exist")
+            // `NodeRef` implements `Deref` for the DOM element type
+            // this means we can call `HtmlInputElement::value()`
+            // to get the current value of the input
+            .value();
+        
+        set_name(value);
+    };
+    view! {
+        <form on:submit=on_submit>
+            <input type="text"
+                value=name
+                node_ref=input_element
+            />
+            <input type="submit" value="Submit"/>
+        </form>
+        <p>"Name is: " {name}</p>
     }
 }
 
